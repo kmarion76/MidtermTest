49b9530aace90ab334be656cb24cbd80f07b8372
brooke <brookedanae88@gmail.com>
2021-02-24 19:28:27 -0500

    Added StreamReader to Array & Menu Class

---
+++ GamersFuel/GamersFuel/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamersFuel
+{
+    public class Menu
+    {
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        public string Price { get; set; }
+
+        public Menu()
+        {
+
+        }
+
+        
+    }
+}
---
+++ GamersFuel/GamersFuel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GamersFuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response;
+            var menuList = new List<string>();
+            do
+            {
+                using (var reader = new StreamReader("Menu.txt"))
+                {
+                    string fileContents;
+                    while ((fileContents = reader.ReadLine()) != null)
+                    {
+                        menuList.Add(fileContents);
+                        string[] menuArray = menuList.ToArray();
+                        menuArray = fileContents.Split('|');
+                        Console.WriteLine(menuArray[2]);
+                    }
+
+                }
+                Console.WriteLine("Would you like to order more? (y/n)");
+                response = Console.ReadLine().ToLower();
+            } while (response == "y");
+
+
+            //Console.WriteLine("What do you want?");
+            //var response = Console.ReadLine().ToLower();
+            //if (response == "n")
+            //{
+            //    Console.WriteLine(arrayMenu[0]);
+            //}
+
+            //Menu myObj = new Menu();
+            //myObj.Name = arrayMenu[0];
+            //myObj.Category = arrayMenu[1];
+            //Console.WriteLine(myObj.Name);
+            //Console.WriteLine(myObj.Category);
+        }
+    }
+}
